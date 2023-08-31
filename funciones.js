7a8631f00a4cb3bb8e90159beeb13a4bde19d02d
Miguel Ardila <miguel_sxe@hotmail.com>
2023-08-30 22:08:29 -0500

    funciones js

---
+++ funciones.js
@@ -0,0 +1,16 @@
+function areaCircuferencia(radio){
+    const pi = 3.1415169;
+    const resultado = pi * radio * radio
+    //console.log(resultado);
+    return resultado;
+}
+
+const rasult = areaCircuferencia(10);
+const rasult1 = areaCircuferencia(30);
+const rasult2 = areaCircuferencia(50);
+const rasult3 = areaCircuferencia(90);
+
+console.log(rasult);
+console.log(rasult1);
+console.log(rasult2);
+console.log(rasult3);
