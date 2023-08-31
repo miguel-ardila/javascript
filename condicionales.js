f7402e0bbb82dcbfbbeebe0e22f3877bcba87387
Miguel Ardila <miguel_sxe@hotmail.com>
2023-08-30 21:09:10 -0500

    condicionales

---
+++ condicionales.js
@@ -0,0 +1,11 @@
+
+
+const nombre = "Ada";
+let edad = 30;
+let nota = 8;
+
+if(nombre === "Ada" && nota > 7){
+    console.log("Hola " + nombre + " Su nota es =" + nota);
+} else {
+    console.log("Nombre no existe");
+}
