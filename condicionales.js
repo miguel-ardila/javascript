c3f8ba3849472d318a42c2d1793cd2d13992b868
Miguel Ardila <miguel_sxe@hotmail.com>
2023-08-30 21:56:59 -0500

    condicionales-practica

--- condicionales.js
+++ condicionales.js
@@ -1,6 +1,4 @@
-
-
-const nombre = "Ada";
+/* const nombre = "Ada";
 let edad = 30;
 let nota = 8;
 
@@ -8,4 +6,26 @@ if(nombre === "Ada" && nota > 7){
     console.log("Hola " + nombre + " Su nota es =" + nota);
 } else {
     console.log("Nombre no existe");
+} */
+
+//--------------------------------------------------------------
+//Practica
+
+const POSITIVO = "positivo";
+const NEGATIVO = "negativo";
+
+let numeroPositivo = 7;
+let numeroNegativo = -5;
+
+if (numeroPositivo > 0){
+    console.log("El numero positivo es " + POSITIVO);
+} else {
+    console.log("El numero positivo no es " + POSITIVO);
 }
+if (numeroNegativo < 0){
+    console.log("El numero negativo es " + NEGATIVO);
+} else {
+    console.log("El numero negativo no es " + NEGATIVO);
+}
+
+
