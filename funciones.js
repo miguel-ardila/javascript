8933065c93e5e8a9607406d2da8274aaaa49a3ec
Miguel Ardila <miguel_sxe@hotmail.com>
2023-09-01 21:03:42 -0500

    funciones

--- funciones.js
+++ funciones.js
@@ -1,4 +1,4 @@
-function areaCircuferencia(radio){
+/*function areaCircuferencia(radio){
     const pi = 3.1415169;
     const resultado = pi * radio * radio
     //console.log(resultado);
@@ -8,9 +8,38 @@ function areaCircuferencia(radio){
 const rasult = areaCircuferencia(10);
 const rasult1 = areaCircuferencia(30);
 const rasult2 = areaCircuferencia(50);
-const rasult3 = areaCircuferencia(90);
-
+ 
 console.log(rasult);
 console.log(rasult1);
 console.log(rasult2);
 console.log(rasult3);
+*/
+//funciones parte 2 
+
+/*function mayorDeDosNUmeros(ingresoUno, ingresoDos){
+    if(ingresoUno > ingresoDos){
+        return (ingresoUno + " Es mayor que " + ingresoDos);
+    } else {
+        return (ingresoDos + " Es mayor que " + ingresoUno);
+    }
+}
+const resultado = mayorDeDosNUmeros (40, 391)
+console.log(resultado + "");
+
+mayorDeDosNUmeros(400, 391);
+
+let mensaje = " Hola clase de javascript "
+console.log(mensaje[2]);
+*/
+//funciones parte 3
+
+function containsWord (mensaje , texto) {
+    const resultado = mensaje.indexOf(texto);
+    console.log(resultado);
+    if (resultado !== -1){
+        console.log(" La palabra " + texto + " se encuentra en el mensaje");
+    } else {
+        console.log("La palabra " + texto + " no se encuentra en el mensaje");
+}
+}
+containsWord("Hola clase de javascript", "clase");
