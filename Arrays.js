e5c7ef953b03c294a252cea6b3477b52ae8490ac
Miguel Ardila <miguel_sxe@hotmail.com>
2023-09-01 21:27:00 -0500

    listas o arrays

---
+++ Arrays.js
@@ -0,0 +1,18 @@
+// listas o Arrays
+
+/*
+
+*/
+
+//ejemplo 1
+
+let autos = [];
+autos [0] = "BMW";
+autos [1] = "Audi";
+autos [2] = "Mercedes";
+autos [3] = "Volkswagen";
+
+
+//ejemplo 2
+
+let frutas = [];
