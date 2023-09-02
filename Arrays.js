ac7fcd9a51e5747a2ddc9d2f5232a5b7b82ea523
Miguel Ardila <miguel_sxe@hotmail.com>
2023-09-01 23:10:09 -0500

    Arrays

--- Arrays.js
+++ Arrays.js
@@ -1,5 +1,5 @@
 // listas o Arrays
-/*
+
 //ejemplo 1
 let autos = [];
 autos [0] = "BMW";
@@ -22,7 +22,7 @@ frutas [2] = "mandarinas"
 let frutas2 = ["Banana", "Pera", "Kiwi", "Mango"];
 
 console.log(frutas2);
-*/
+
 //Practica Declara 1 arreglo llamado miFamilia o myFamily.
 
 let miFamilia = ["Miguel", "Amy", "Stefanny"];
